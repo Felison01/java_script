4816fa85b30a4843093dcd0e54e18c13019fb8e9
Felison01 <felisonfernandes@gmail.com>
2023-10-06 21:14:29 +0530

    tst

---
+++ operator/multiply.js
@@ -0,0 +1,14 @@
+var a = 10
+var b = 20.5
+var c = '30'
+var d = 'rahul'
+var e = true 
+var f;
+console.log(a*b)  //205
+console.log(a*c) //300
+console.log(a*d) //NaN
+console.log(a*e) //10
+console.log(a*f) //NaN
+console.log(typeof e)
+console.log(typeof c)
+console.log(typeof f)
