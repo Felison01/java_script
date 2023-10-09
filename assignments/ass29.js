8524c263cfb439c593cf63c6f92b329d0767bb38
Felison01 <felisonfernandes@gmail.com>
2023-10-09 21:37:19 +0530

    tst

---
+++ assignments/ass29.js
@@ -0,0 +1,12 @@
+let a = 200;
+let b = 55;
+let c = 3323;
+if(a<b && a<c){
+  console.log('a is least');
+}
+else if(b<c){
+  console.log('b is least');
+}
+else {
+  console.log('c is least')
+}
---
+++ assignments/ass33.js
@@ -0,0 +1,2 @@
+let num = 71;
+num % 7 === 0 ? console.log('is divisible'): console.log('not divisible');
