70810ec0ad15e643f74aa17722205d4e644de7cd
Felison01 <felisonfernandes@gmail.com>
2023-10-11 15:55:50 +0530

    tst

--- assignments_iterative/ass49.js
+++ assignments_iterative/ass49.js
@@ -10,4 +10,4 @@ for(let i =0 ;i<10;i++)
   if(i % 2 === 0)
   {console.log(i+1);}
 }
-*/
+*/          
---
+++ assignments_iterative/ass57.js
@@ -0,0 +1,10 @@
+let i = 1;
+let j = 1
+while(i<=60){
+  if(i % 6 === 0)
+  {console.log(`6 * ${j} = ${i} `)
+  j++
+}
+i++
+
+}
