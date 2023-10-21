5c40f497d231569d2430d3bf02a9cd043dcbf5a0
Felison01 <felisonfernandes@gmail.com>
2023-10-21 13:52:20 +0530

    tst

---
+++ array/one.js
@@ -0,0 +1,32 @@
+let arr = [20,30,40,50]
+let arr1 = arr.pop()
+console.log(arr1)
+console.log(arr)
+
+
+arr.push(60)
+console.log(arr)
+console.log(arr.length)
+
+arr.shift()
+arr.unshift(10)
+console.log(arr)
+
+let a = arr.toString()     //a is now a string
+console.log(a)
+
+ let b = arr.join("_")
+ console.log(b,typeof b)
+
+ let arr2 = [10,20,30,40,50,1,2,3,4]
+ let c = arr2.filter((value)=>{
+  return value<20
+ })
+
+ console.log(c)
+ console.log(arr2)
+
+ let d = arr2.map((maped)=>{
+  return maped + 10
+ })
+ console.log(d)
