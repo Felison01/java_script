e4c2ac67e2c10175f924bf40f2cb6d8c0660c2dc
Felison01 <felisonfernandes@gmail.com>
2023-10-22 21:31:09 +0530

    tsttt

---
+++ import_export/one.js
@@ -0,0 +1,13 @@
+ let a = ()=>{
+  console.log('hello world')
+}
+let b =()=>{
+  console.log('hello')
+  return 1+ 2;
+}
+console.log(c = b())
+a()
+
+
+
+module.export = {b,a};
---
+++ import_export/two.js
@@ -0,0 +1,2 @@
+const {b,a} = require('./one')
+
